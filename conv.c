271d6841166e99d9fdee144c7d1a6927b447aea9
Baptiste FROCHOT <bfrochot@e2r6p12.42.fr>
2016-12-04 16:46:40 +0100

    unicode

---
+++ conv.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfrochot <bfrochot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/04 15:49:52 by bfrochot          #+#    #+#             */
+/*   Updated: 2016/12/04 16:45:51 by bfrochot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+#include <stdio.h>
+
+// int		conv(int nb, char *new)
+// {
+// 	char	*nbr;
+// 	int		len;
+
+// 	nbr = ft_itoa_base(nb, 2, 0);
+// 	len = ft_strlen(nbr);
+// 	if (len < 8)
+// 	{
+// 		new[0] = ft_atoi_base_printf(nbr, 2, 0);
+// 	}
+// 	else if (len < 12)
+// 	{
+// 		new[1] = ft_atoi_base_printf(nbr + len - 6, 2, 0) + 128;
+// 		nbr[len - 6] = 0;
+// 		new[0] = ft_atoi_base_printf(nbr, 2, 0) + 192;
+// 	}
+// 	else if (len < 17)
+// 	{
+// 		new[2] = ft_atoi_base_printf(nbr + len - 6, 2, 0) + 128;
+// 		nbr[len - 6] = 0;
+// 		new[1] = ft_atoi_base_printf(nbr + len - 12, 2, 0) + 128;
+// 		nbr[len - 12] = 0;
+// 		new[0] = ft_atoi_base_printf(nbr, 2, 0) + 224;
+// 	}
+// 	else if (len < 22)
+// 	{
+// 		new[3] = ft_atoi_base_printf(nbr + len - 6, 2, 0) + 128;
+// 		nbr[len - 6] = 0;
+// 		new[2] = ft_atoi_base_printf(nbr + len - 12, 2, 0) + 128;
+// 		nbr[len - 12] = 0;
+// 		new[1] = ft_atoi_base_printf(nbr + len - 18, 2, 0) + 128;
+// 		nbr[len - 18] = 0;
+// 		new[0] = ft_atoi_base_printf(nbr, 2, 0) + 240;
+// 	}
+// 	else
+// 		return (-1);
+// 	return (1);
+// }
+
+int		ft_count(int len)
+{
+	if (len < 8)
+		return (0);
+	else if (len < 12)
+		return (1);
+	else if (len < 17)
+		return (2);
+	else
+		return (3);
+}
+
+int		plus(int i)
+{
+	if (i == 3)
+		return (240);
+	else if (i == 2)
+		return (224);
+	else if (i == 1)
+		return (192);
+	else
+		return (0);
+}
+
+int		conv(int nb, char *new)
+{
+	char	*nbr;
+	int		len;
+	int		k;
+	int		i;
+
+	nbr = ft_itoa_base(nb, 2, 0);
+	len = ft_strlen(nbr);
+	if (len < 22)
+	{
+		k = 6;
+		i = ft_count(len);
+		while (i > 0)
+		{
+			new[i] = ft_atoi_base_printf(nbr + len - k, 2, 0) + 128;
+			nbr[len - k] = 0;
+			--i;
+			k += 6;
+		}
+		new[0] = ft_atoi_base_printf(nbr, 2, 0) + plus(k / 6 - 1);
+	}
+	else
+		return (-1);
+	return (1);
+}
+
+int		main()
+{
+	char *new;
+
+	new = malloc(4);
+	ft_bzero(new, 4);
+	conv(0xa6a8, new);
+	write(1, new, 4);
+	return (0);
+}
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/03 16:11:27 by mleclair          #+#    #+#             */
+/*   Created: 2016/12/03 106:11:27 by mleclair          #+#    #+#             */
 /*   Updated: 2016/12/03 19:23:47 by mleclair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -17,16 +17,17 @@ int   main()
 {
   int ret1 = 0;
   int ret2 = 0;
-  char c[4];
+  char c[5];
 
 
 //  printf("Locale is: %s\n", setlocale(LC_ALL, "en_US"));
-  ret1 = printf("vrai = %08x\n", L'ሴ');
-    c[0] = 0xe1;
-    c[1] = 0x88;
-    c[2] = 0xb4;
-    c[3] = 0x61;
-    write(1, c, 4);
+  ret1 = printf("vrai = %c\n", 'a');
+    c[0] = 0xf0;
+    c[1] = 0x81;
+    c[2] = 0x88;
+    c[3] = 0xb4;
+    c[4] = 0x00;
+    write(1, c, 5);
  // ret2 = ft_printf("notr = %lc\n", L'莨');
   printf("ret1 = %d, ret2 = %d\n\n", ret1, ret2);
 }
